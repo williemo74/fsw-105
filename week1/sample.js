54f2880a0e3d4e533f1a9a6874ba2438810efb45
williemo74 <willie.morgan@bryanuniversity.edu>
2020-02-28 04:32:30 -0600

    initial

---
+++ week1/sample.js
@@ -0,0 +1,42 @@
+var name = "James"; // This comment appears at tje end of the line
+
+//This is my in line comment
+
+/*
+
+This is an example of my block comment
+
+var person - {
+
+var subtotal=75;
+subtotal +=25; // subtotal = subtotal +25;
+
+number + number
+
+25 + 25 = 50
+"Months: " + 50; // Months: 50
+
+console.log("Month is: " + subtotal + " seven")
+
+}
+
+
+var rate = 5;
+
+if (rate > 12)
+{
+    // code block
+}
+else if (rate < 20)
+{
+    // code
+}
+
+for (counter=1; counter < 5; counter ++)
+{
+    console.log("Counter Value: " + counter)
+}
+
+if ()
+
+*/
