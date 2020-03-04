adc0578dfc53834bfaac2dbace2bd566366feaa9
williemo74 <willie.morgan@bryanuniversity.edu>
2020-03-04 12:12:59 -0600

    Update shopper.js

---
+++ week1/shopper.js
@@ -0,0 +1,20 @@
+var shopper = {
+    firstName: "Crystal",
+    lastName: "White",
+    memberId: 1234,
+    fullName: function() {
+        return this.firstName + "Crystal White" + this.lastName;
+    }
+};
+var message = "See your items"
+Object.create(groccerycart) 
+{
+    itemName: "pork", "carrots"
+    itemType: "meat", "vegetables"
+    itemCost: cost + + subtotal
+    itemTax: taxIs (0.10) 
+    Costis (cost + Subtotal + tax 
+        )
+    
+
+}
